1b9c808f724955720f64da302b46caa45c4ac3c9
LautaroOlmedo <72723456+LautaroOlmedo@users.noreply.github.com>
2022-04-11 10:54:08 -0300

    Inicio Editor

---
+++ Editor_I.java
@@ -0,0 +1,20 @@
+package EditorDeTexto;
+import java.awt.*;
+
+import javax.swing.JFrame;
+
+public class Editor_I{
+    public static void main(String[] args) {
+        
+    }
+}
+
+class marco extends JFrame{
+
+    public marco(){
+        setVisible(true);
+        setLocationRelativeTo(null);
+    
+    }
+
+}
