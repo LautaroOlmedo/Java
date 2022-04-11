67526d36492065c52dd2f20b5560af4ac6b75dde
LautaroOlmedo <72723456+LautaroOlmedo@users.noreply.github.com>
2022-04-11 10:57:15 -0300

    Agregado marco y frame

--- Editor_I.java
+++ Editor_I.java
@@ -1,10 +1,13 @@
 package EditorDeTexto;
 import java.awt.*;
-
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 public class Editor_I{
     public static void main(String[] args) {
+        marco mimarco = new marco();
+        mimarco.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
         
     }
 }
@@ -14,7 +17,21 @@ class marco extends JFrame{
     public marco(){
         setVisible(true);
         setLocationRelativeTo(null);
+        setTitle("Editor I");
+        setResizable(true);
+        setSize(400, 350);
+
+
+        frame miframe = new frame();
+        add(miframe);
     
     }
 
+}
+
+class frame extends JPanel{
+    public frame(){
+
+    }
+    
 }
