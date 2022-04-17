10f179485402fdc472039c6a74b8fe74d0fab5d0
LautaroOlmedo <72723456+LautaroOlmedo@users.noreply.github.com>
2022-04-17 11:37:26 -0300

    Add files via upload

--- practicas/Lamina_Dialogos.java
+++ practicas/Lamina_Dialogos.java
@@ -8,16 +8,25 @@ public class Lamina_Dialogos extends JPanel{
 
         setBorder(BorderFactory.createTitledBorder(BorderFactory.createEtchedBorder(), titulo));
         setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
-
-        ButtonGroup grupo = new ButtonGroup();
+        grupo = new ButtonGroup();
 
         for(int i = 0; i < opciones.length; i ++){
 
             JRadioButton bot = new JRadioButton(opciones[i]);
+            bot.setActionCommand(opciones[i]);
             add(bot);
             grupo.add(bot);
             bot.setSelected(i == 0);
         }
     }
+
+    public String dameSeleccion(){
+
+        /*ButtonModel  miboton = grupo.getSelection(); // ALMACENA EL JRADIOBUTTON SELECCIONADO
+        String s = miboton.getActionCommand();  //DEVUELVE EL STRING DE LA ACCIÓN DE COMANDO
+        return s;*/
+        return grupo.getSelection().getActionCommand(); 
+    }
+    ButtonGroup grupo;
 }  
 
--- practicas/Marco_Dialogos.java
+++ practicas/Marco_Dialogos.java
@@ -1,6 +1,9 @@
 package practicas;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.*;
+import java.util.Date;
+import java.awt.geom. Rectangle2D;
 
 public class Marco_Dialogos extends JFrame {
 
@@ -76,14 +79,79 @@ public class Marco_Dialogos extends JFrame {
 
         laminaFondo = new JPanel();
         JButton mostrar = new JButton("Mostrar");
+        mostrar.addActionListener(new AccionMostrar());
         laminaFondo.add(mostrar);
         add(laminaFondo, BorderLayout.SOUTH);
         
         //------------------------------------------
         add(laminaCuadricula, BorderLayout.CENTER);
     }
+    
+    public Object dameMensaje(){
+
+        String s = lamina_mensaje.dameSeleccion();
+        if(s.equals("Cadena")){
+            return cadenaMensaje;
+
+        }else if(s.equals("Icono")){
+            return iconMensaje;
+
+        }else if(s.equals("Componente")){
+            return componenteMensaje;
+
+        }else if(s.equals("Otros")){
+            return objetoMensaje;
+
+        }else if(s.equals("Objects[]")){
+            return new Object[]{
+                cadenaMensaje,
+                componenteMensaje,
+                objetoMensaje
+            };
+        }else{
+            return null;
+        }
+    }
+
+    //------------------------------------------ CLASE INTERNA QUE GESTIONA EVENTOS
+    private class AccionMostrar implements ActionListener{
+
+        public void actionPerformed(ActionEvent e){
+
+            if(lamina_tipo.dameSeleccion().equals("Mensaje")){
+                JOptionPane.showMessageDialog(Marco_Dialogos.this, dameMensaje(), "Titulo", 0);
+
+            }else if(lamina_tipo.dameSeleccion().equals("Confirmar")){
+                JOptionPane.showConfirmDialog(Marco_Dialogos.this, dameMensaje(), "Titulo", 0, 0);
+
+            }else if(lamina_tipo.dameSeleccion().equals("Entrada")){
+                JOptionPane.showInputDialog(Marco_Dialogos.this, dameMensaje(), "Titulo", 0);
+
+            }else if(lamina_tipo.dameSeleccion().equals("Objects[]")){
+                JOptionPane.showOptionDialog(Marco_Dialogos.this, dameMensaje(), "Titulo", 0, 0, null, null, null);
+
+            }
+        }
+
+    }
 
     private Lamina_Dialogos lamina_tipo, lamina_tipomensaje, lamina_mensaje, lamina_confirmar, lamina_opcion, lamina_entrada;
     private JPanel laminaFondo;
+    private String cadenaMensaje = "Mensaje";
+    private Icon iconMensaje = null;
+    private Object objetoMensaje = new Date();
+    private Component componenteMensaje = new Lamina_Ejemplo();
        
+}
+
+class Lamina_Ejemplo extends JPanel{
+    
+    public void paintComponent(Graphics g){
+
+        super.paintComponent(g);
+        Graphics2D g2 = (Graphics2D) g;
+        Rectangle2D rectangulo = new Rectangle2D.Double(0, 0, getWidth(), getHeight());
+        g2.setPaint(Color.YELLOW);
+        g2.fill(rectangulo);
+    }
 }
